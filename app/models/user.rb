40349d2cf876e4764a4e3a50ae9800459ef0980a
ashwini <ashwini.mandal@cuelogic.com>
2018-06-25 18:09:45 +0530

    working functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,4 @@
 class User < ActiveRecord::Base
-    
-    def self.connet_to_gmail
-        require 'gmail'
-        user=User.first
-        gmail=Gmail.connect(:xoauth2,user.email,user.token)
-        if gmail.logged_in?
-            puts "success"
-        else
-            puts "fail"
-        end
-
-    end
+    validates :email,presence: true
+    #validates :name, presence: true
 end
